3ab45eed4fefd8ef4681655107a6ce9f6a7b82d8
Cll <2642137848@qq.com>
2021-01-21 17:32:09 +0800

    尝试在gitup上发布

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default function () {
+  return {
+    // 入口
+    entry: 'index.html',
+    // 出口
+    outDir: 'docs',
+    // 打包后的跟路径
+    base: '/html/',
+    // 输出的静态资源的文件夹名称
+    assetsDir: 'assets',
+    base: '/number-hrd/'
+  }
+}
