0fbaeba0c4eccc66b7d4e1aff9c36d8f5fd304c5
cll123456 <41314419+cll123456@users.noreply.github.com>
2021-01-21 17:49:27 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ export default function () {
     entry: 'index.html',
     // 出口
     outDir: 'docs',
-    // 打包后的跟路径
-    base: '/html/',
     // 输出的静态资源的文件夹名称
     assetsDir: 'assets',
     base: '/number-hrd/'
